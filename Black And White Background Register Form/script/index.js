3c35278ede8973acc18ce9dc500fb5ee1357a8f8
Atalay Sezen <88587309+AtalaySezen@users.noreply.github.com>
2022-04-12 21:18:19 +0300

    Black And White Background Register Page

---
+++ Black And White Background Register Form/script/index.js
@@ -0,0 +1,19 @@
+const loginButton = document.getElementById('button-login');
+
+
+
+function registerHref(){
+    location.href = "register.html"
+}
+
+  function loginHref(){
+      location.href = "login.html"
+  }
+
+
+
+
+AOS.init();
+
+console.log('%c Camper Finder Made By Atalay ', 'background: #000; color: #fff');
+
